c041fbac86ff61eb96d522bb2aa2c8787f5e8563
Dominik Wagner <DomiWag@users.noreply.github.com>
2013-10-31 11:14:52 +0100

    Add basic django project

---
+++ tsune/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tsune.views.home', name='home'),
+    # url(r'^tsune/', include('tsune.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('djan
+    # Uncomment the next line to enable the admin:go.contrib.admindocs.urls')),
+
+    #url(r'^admin/', include(admin.site.urls)),
+)
