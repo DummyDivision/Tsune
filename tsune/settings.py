9459dc64605f1246375022e4c06fe171dc4f6065
Dominik Wagner <DomiWag@users.noreply.github.com>
2013-11-04 10:17:49 +0100

    Fix for unset DATABASE_URL

--- tsune/settings.py
+++ tsune/settings.py
@@ -158,10 +158,12 @@ LOGGING = {
 }
 
 # Settings for Heroku Deployment
+import os
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+if os.environ.get("DATABASE_URL") != None:
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -170,7 +172,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
