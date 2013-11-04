f2b6988cd065d85d0bcb59bf416a00a9a308a9cc
Dominik Wagner <DomiWag@users.noreply.github.com>
2013-11-04 10:20:21 +0100

    Configure database defaults for vagrant

--- tsune/settings.py
+++ tsune/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'NAME': 'vagrant',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
+        'USER': 'vagrant',
+        'PASSWORD': 'vagrant',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
