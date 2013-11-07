242e8c1f1efdc3f779687a57ea015e0bac4346a7
Andreas Waigand <awaigand@gmx.net>
2013-11-07 22:01:50 +0100

    Register cardbox app in settings.py

--- tsune/settings.py
+++ tsune/settings.py
@@ -139,6 +139,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cardbox',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
