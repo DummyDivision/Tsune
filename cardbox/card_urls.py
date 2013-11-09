5113f21eea2552f4bece1fa1a8d06c89b96eae51
Andreas Waigand <awaigand@gmx.net>
2013-11-09 14:49:44 +0100

    Remove test urls from urls.py

--- cardbox/card_urls.py
+++ cardbox/card_urls.py
@@ -1,6 +1,5 @@
-from django.conf.urls import patterns, include, url
-import card_views
+from django.conf.urls import patterns, url
+from cardbox import card_views
 
 urlpatterns = patterns('',
-                       url(r'^$', card_views.index, name='index'),
 )
