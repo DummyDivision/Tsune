abab24903935d2b70a160eb491de9a4a90c43154
Andreas Waigand <awaigand@gmx.net>
2013-11-09 14:51:16 +0100

    Add generic delete view

--- cardbox/card_urls.py
+++ cardbox/card_urls.py
@@ -2,4 +2,7 @@ from django.conf.urls import patterns, url
 from cardbox import card_views
 
 urlpatterns = patterns('',
+                       url(r'^delete/(?P<pk>\d+)$', card_views.CardDelete.as_view(
+                           template_name="cardbox/card/card_confirm_delete.html"),
+                           name='card_delete'),
 )
--- cardbox/card_views.py
+++ cardbox/card_views.py
@@ -1,1 +1,23 @@
+# coding=utf-8
+# TODO: user authentification
+# TODO: comments
+from django.http import HttpResponseForbidden
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.core.urlresolvers import reverse_lazy
+from cardbox.card_forms import CardForm
+
+from cardbox.card_model import Card
+
+class CardDelete(DeleteView):
+    model = Card
+    def get(self, request, *args, **kwargs):
+        """Never get the confirm_delete template!"""
+        print "test"
+        return HttpResponseForbidden("<h1>Access Denied</h1>")
+
+    def post(self, *args, **kwargs):
+        #TODO: user authentification
+        return super(CardDelete, self).post(self, args, kwargs)
 
