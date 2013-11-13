139bb1693c7f8e17603cfe9db5f6f9b28fda200e
Manuel Batsching <flammschild@gmail.com>
2013-11-13 19:37:40 +0100

    Activate admin site
    Django's built in admin site is active now and can be found at
    /admin/. Don't forget to run a syncdb.

--- tsune/settings.py
+++ tsune/settings.py
@@ -142,7 +142,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'cardbox',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
--- tsune/urls.py
+++ tsune/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -13,7 +13,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # Uncomment the next line to enable the admin:go.contrib.admindocs.urls')),
 
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^cardbox/deck/', include('cardbox.deck_urls', namespace="deck")),
     url(r'^cardbox/card/', include('cardbox.card_urls', namespace="card")),
 )
