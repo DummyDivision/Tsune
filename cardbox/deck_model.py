5ed1f6656f4f41278334663d5b7646c84015e815
Dominik Wagner <DomiWag@users.noreply.github.com>
2013-11-13 11:14:49 +0100

    Add deck model

--- cardbox/deck_model.py
+++ cardbox/deck_model.py
@@ -1,1 +1,18 @@
-# TODO: Add model for decks
+# TODO: Connection with card
+from django.db import models
+from django.core.urlresolvers import reverse
+
+
+class Deck(models.Model):
+    ID = models.AutoField(primary_key=True)
+    # Cards are realized as Many to Many in the card model
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=512)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the unique url to this object"""
+        return reverse('deck:deck_detail', kwargs={'pk': self.ID})
