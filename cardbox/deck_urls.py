256bfb9e2d04fbd03ec2f4d3551e8a9d5ae11766
Dominik Wagner <DomiWag@users.noreply.github.com>
2013-11-14 12:11:22 +0100

    Add URL rules for deck CRUD

--- cardbox/deck_urls.py
+++ cardbox/deck_urls.py
@@ -3,5 +3,16 @@ import deck_views
 
 
 urlpatterns = patterns('',
-                       url(r'^$', deck_views.index, name='index'),
+                       url(r'^$',
+                           deck_views.DeckList.as_view(template_name="cardbox/deck/deck_list.html"),
+                           name='deck_list'),
+                       url(r'^new$', deck_views.DeckCreate.as_view(
+                           template_name="cardbox/deck/deck_form.html"), name='deck_new'),
+                       url(r'^edit/(?P<pk>\d+)$', deck_views.DeckUpdate.as_view(
+                           template_name="cardbox/deck/deck_form.html"), name='deck_edit'),
+                       url(r'^delete/(?P<pk>\d+)$', deck_views.DeckDelete.as_view(
+                           template_name="cardbox/deck/deck_confirm_delete.html"),
+                           name='deck_delete'),
+                       url(r'^detail/(?P<pk>\d+)/$', deck_views.DeckDetailView.as_view(
+                           template_name="cardbox/deck/deck_detail.html"), name='deck_detail')
 )
