689de21110377c978d3a23a2c1d78a2d626103c6
Dominik Wagner <DomiWag@users.noreply.github.com>
2013-11-14 11:39:01 +0100

    Make delete popup more general

--- cardbox/static/cardbox/js/show_delete_popup.js
+++ cardbox/static/cardbox/js/show_delete_popup.js
@@ -1,14 +1,14 @@
 function show_delete_popup(path) {
-    if ($("#card_delete_popup").is(':visible')) {
+    if ($("#delete_popup").is(':visible')) {
         return;
     }
-    $("#card_delete_popup_form").get(0).setAttribute('action', path);
-    $("#card_delete_popup").show()
+    $("#delete_popup_form").get(0).setAttribute('action', path);
+    $("#delete_popup").show()
 }
 
 function hide_delete_popup() {
-    if ($("#card_delete_popup").is(':visible')) {
-        $("#card_delete_popup_form").get(0).setAttribute('action', '#');
-        $("#card_delete_popup").hide()
+    if ($("#delete_popup").is(':visible')) {
+        $("#delete_popup_form").get(0).setAttribute('action', '#');
+        $("#delete_popup").hide()
     }
 }
