8ac86084fded1f7ed82e3a38c84498f9d814fbaa
Andreas Waigand <awaigand@gmx.net>
2013-11-18 21:42:13 +0100

    Replace author string

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Tsune'
-copyright = u'2013, Manuel Batsching, Jasmin Marquardt, Dominik Wagner, Andreas Waigand'
+copyright = u'2013, dummydivision'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -184,7 +184,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
   ('index', 'Tsune.tex', u'Tsune Documentation',
-   u'Manuel Batsching, Jasmin Marquardt, Dominik Wagner, Andreas Waigand', 'manual'),
+   u'dummydivision', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -214,7 +214,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'tsune', u'Tsune Documentation',
-     [u'Manuel Batsching, Jasmin Marquardt, Dominik Wagner, Andreas Waigand'], 1)
+     [u'dummydivision'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -228,7 +228,7 @@ man_pages = [
 #  dir menu entry, description, category)
 texinfo_documents = [
   ('index', 'Tsune', u'Tsune Documentation',
-   u'Manuel Batsching, Jasmin Marquardt, Dominik Wagner, Andreas Waigand', 'Tsune', 'One line description of project.',
+   u'dummydivision', 'Tsune', 'One line description of project.',
    'Miscellaneous'),
 ]
 
