e6bbf3084df6cca4c702740347a2974698a2c751
Dominik Wagner <DomiWag@users.noreply.github.com>
2013-11-19 14:19:39 +0100

    Require login for deck operations

--- cardbox/deck_urls.py
+++ cardbox/deck_urls.py
@@ -1,18 +1,20 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 import deck_views
 
 
 urlpatterns = patterns('',
                        url(r'^$',
-                           deck_views.DeckList.as_view(template_name="cardbox/deck/deck_list.html"),
-                           name='deck_list'),
-                       url(r'^new$', deck_views.DeckCreate.as_view(
-                           template_name="cardbox/deck/deck_form.html"), name='deck_new'),
-                       url(r'^edit/(?P<pk>\d+)$', deck_views.DeckUpdate.as_view(
-                           template_name="cardbox/deck/deck_form.html"), name='deck_edit'),
-                       url(r'^delete/(?P<pk>\d+)$', deck_views.DeckDelete.as_view(
-                           template_name="cardbox/deck/deck_confirm_delete.html"),
+                           login_required(deck_views.DeckList.as_view(
+                               template_name="cardbox/deck/deck_list.html")), name='deck_list'),
+                       url(r'^new$', login_required(deck_views.DeckCreate.as_view(
+                           template_name="cardbox/deck/deck_form.html")), name='deck_new'),
+                       url(r'^edit/(?P<pk>\d+)$', login_required(deck_views.DeckUpdate.as_view(
+                           template_name="cardbox/deck/deck_form.html")), name='deck_edit'),
+                       url(r'^delete/(?P<pk>\d+)$', login_required(deck_views.DeckDelete.as_view(
+                           template_name="cardbox/deck/deck_confirm_delete.html")),
                            name='deck_delete'),
-                       url(r'^detail/(?P<pk>\d+)/$', deck_views.DeckDetailView.as_view(
-                           template_name="cardbox/deck/deck_detail.html"), name='deck_detail')
+                       url(r'^detail/(?P<pk>\d+)/$', login_required(
+                           deck_views.DeckDetailView.as_view(
+                               template_name="cardbox/deck/deck_detail.html")), name='deck_detail')
 )
