e4b47c9bc3de18c83a2fb718c806b7668b492de6
Andreas Waigand <awaigand@gmx.net>
2013-11-19 23:35:14 +0100

    Add name to logout url regex

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,4 +3,4 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('django.contrib.auth.views',
 
     url(r'^login/$', 'login', {'template_name': 'authentication/login.html'}),
-    url(r'^logout/$', 'logout_then_login'))
+    url(r'^logout/$', 'logout_then_login',name='logout'))
