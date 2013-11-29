967e8e2e1529fea93a8867a7026e85f019c95288
Andreas Waigand <awaigand@gmx.net>
2013-11-29 06:54:49 +0100

    Add deckglue app

--- tsune/settings.py
+++ tsune/settings.py
@@ -144,6 +144,7 @@ INSTALLED_APPS = (
     'guardian',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'deckglue',
     'authentication',
     'cardbox',
 )
