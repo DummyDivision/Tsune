309f107a9309bd563e89c7bb49d35a02eed5bcc7
Andreas Waigand <awaigand@gmx.net>
2014-04-04 01:34:39 +0200

    Add SECRETS_PRESENT bool to login.html context

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import patterns, include, url
+from authentication import views
 
 urlpatterns = patterns('django.contrib.auth.views',
 
-    url(r'^login/$', 'login', {'template_name': 'authentication/login.html'}),
+    url(r'^login/$', views.login, {'template_name': 'authentication/login.html'}),
     url(r'^logout/$', 'logout_then_login',name='logout'),
-    url('', include('social.apps.django_app.urls', namespace='social')))
+    url('', include('social.apps.django_app.urls', namespace='social')),
+    url('', include('django.contrib.auth.urls')))
--- authentication/views.py
+++ authentication/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.generic import TemplateView, FormView
+from django.contrib.auth import views
+from tsune  import settings
+
+def login(request,**kwargs):
+    response = views.login(request,kwargs.get('template_name'));
+    response.context_data['secrets_present'] = settings.SECRETS_PRESENT;
+    return response;
