976b48ea133881a98bb2ee76edcf4afa3291ac7e
Andreas Waigand <awaigand@gmx.net>
2014-04-14 13:35:04 +0200

    Fixed Has No Context_Data Attribute for login

--- authentication/views.py
+++ authentication/views.py
@@ -6,5 +6,8 @@ from tsune  import settings
 
 def login(request,**kwargs):
     response = views.login(request,kwargs.get('template_name'));
+    if not hasattr(response, 'context_data'):
+        response.context_data = {'secrets_present':settings.SECRETS_PRESENT}
+        return response
     response.context_data['secrets_present'] = settings.SECRETS_PRESENT;
     return response;
