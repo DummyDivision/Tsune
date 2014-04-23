f6699dea7a774421e4da587106684f08c44000d5
Dominik Wagner <DomiWag@users.noreply.github.com>
2014-04-23 16:30:18 +0200

    TSUNE-105 Fix small problem related to DATABASE_URL

--- tsune/settings/heroku.py
+++ tsune/settings/heroku.py
@@ -1,6 +1,7 @@
 from .base import *
 
-if get_env_variable("DATABASE_URL") is not None:
-    # Parse database configuration from $DATABASE_URL
-    import dj_database_url
-    DATABASES['default'] =  dj_database_url.config()
+get_env_variable("DATABASE_URL")
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
