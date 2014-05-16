a9a60879a8ba48ab6a01a58c5229cd522cf051e3
Andreas Waigand <awaigand@gmx.net>
2014-05-16 22:29:50 +0200

    TSUNE-79 Add additional functionality to source reader

--- cardimporter/sourcereader.py
+++ cardimporter/sourcereader.py
@@ -1,11 +1,26 @@
 from zipfile import ZipFile
 from sqlite3 import connect
 from os.path import dirname
+from simplejson import loads
 
 class AnkiCardSourceReader():
 
-    def __init__(self,pathToAnkiArchive):
-        self.pathToAnkiArchive = pathToAnkiArchive
+    def __init__(self, pathToApkg):
+        self.pathToApkg = pathToApkg
+
+    def __enter__(self):
+        pathToCollection = self._extractSqlLiteDBFromAnkiArchive(self.pathToApkg)
+        self._connectToSqlLiteDB(pathToCollection)
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._disconnectFromDb()
+
+    def _connectToSqlLiteDB(self, pathToDb):
+        self.connection = connect(pathToDb)
+
+    def _disconnectFromDb(self):
+        self.connection.close()
 
     def _extractSqlLiteDBFromAnkiArchive(self, path):
         apkgFile = ZipFile(path)
@@ -13,20 +28,31 @@ class AnkiCardSourceReader():
         apkgFile.close()
         return dirname(path)+'''/collection.anki2'''
 
-    def _getListOfAnkiCardStringFromAnkiSqliteDb(self, pathToSqliteDb):
-        con = connect(pathToSqliteDb)
-        cur = con.cursor()
-        cur.execute("SELECT flds from notes")
-        listofAnkiCards = cur.fetchall()
-        con.close()
-        #Since this returns tuple such as ('front\x1f\x1fback','') a fix is needed
-        fixedListOFAnkiCards = [card[0] for card in listofAnkiCards]
-        return fixedListOFAnkiCards
+    def _selectOnDb(self, query):
+        cur = self.connection.cursor()
+        cur.execute(query)
+        return cur.fetchall()
 
-    def getRawListOfAnkiCardStrings(self, pathToApkg):
-        pathToCollection = self._extractSqlLiteDBFromAnkiArchive(pathToApkg)
-        list_raw = self._getListOfAnkiCardStringFromAnkiSqliteDb(pathToCollection)
-        list_frontback = [card.split('\x1f\x1f') for card in list_raw]
-        return list_frontback
+    def _fixRawListOfAnkiCards(self, rawAnkiCards):
+        # Since this returns tuple such as ('front\x1f\x1fback','') a fix is needed to remove empty element
+        # Split at \xf1\xf1 to get back
+        #return [htmlParser.unescape(card[0]).split('\x1f',1) for card in rawAnkiCards]
+        return [card[0].split('\x1f',1) for card in rawAnkiCards]
+
+    def getRawListOfAnkiCardTuples(self):
+        """Returns list of [front, back] lists in anki html format for all cards"""
+        listofAnkiCards = self._selectOnDb("SELECT flds from notes")
+        return self._fixRawListOfAnkiCards(listofAnkiCards)
+
+    def getAllCardsForDeck(self,deckid):
+        """Returns list of [front, back] lists in anki html format for all cards in deck with deckid"""
+        listofAnkiCards = self._selectOnDb("SELECT flds from notes,cards WHERE notes.id is cards.nid and cards.did = " + deckid)
+        return self._fixRawListOfAnkiCards(listofAnkiCards)
+
+    def getDictOfAllDecks(self):
+        """Returns dictionary of all decks in apkg."""
+        deckDictJson = self._selectOnDb("SELECT decks from col")[0][0]
+        return loads(deckDictJson)
+
 
 
